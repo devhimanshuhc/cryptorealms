2f209827f0c9da54baccebcdb5dbcb28acbc083e
Himanshu <devhimanshuhc@gmail.com>
2022-08-02 21:12:12 +0530

    second commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeader = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "5960ce78d9mshb4803669a0d260dp13ba57jsn3dd0395d5f8c",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({
+  url,
+  headers: cryptoApiHeader,
+});
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
